4435f15192cf358cd9c07ed90b319c7150726e59
Pradeep Kumar <86669152+PradeepMalviya90@users.noreply.github.com>
2021-07-01 16:48:31 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,11 +1,11 @@
 <?php
-header("Location:./profile/index.php");
-$target_dir = "./uploads/";
+
+$target_dir = "pdf/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-
+$file_tmp = $_FILES["fileToUpload"]["tmp_name"];
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 10000000) {
     echo "Sorry, your file is too large.";
@@ -17,7 +17,7 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
     // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    if (move_uploaded_file($file_tmp, $target_file)) {
         echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
