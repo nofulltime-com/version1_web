7cbeabc00245dd482be781d4d09bfb916b7e1a55
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-07-03 16:37:37 +0530

    Making the seeker dashboard dynamic

--- profile/index.php
+++ profile/index.php
@@ -1,13 +1,5 @@
-<?php
-?>
-
-
-
-
-
-
-
 <!DOCTYPE html>
+
 <html lang="en">
 
 <head>
@@ -33,6 +25,68 @@
 </head>
 
 <body>
+	<?php
+
+	$id = 7;
+	include "../connect.php";
+	$query1 = "SELECT * FROM `seeker_details` WHERE id='$id	'";
+	$res1 = $conn->query($query1);
+	if ($res1->num_rows > 0) {
+		while ($row = $res1->fetch_assoc()) {
+			$name = ucfirst($row['fullname']);
+			$age =  $row['age'];
+			$gender = ucfirst($row['gender']);
+			$status = ucfirst($row['status']);
+			$city = ucfirst($row['town']);
+			$state = ucfirst($row['state']);
+			$country = ucfirst($row['country']);
+			$address = $city . "," . $state . "," . $country;
+			$pin_code = $row['pin_code'];
+			$category = $row['category'];
+			$part_time_start = substr($row['part_time_start'], 0, 5);
+			$part_time_end = substr($row['part_time_end'], 0, 5);
+			$course_time_start = substr($row['course_time_start'], 0, 5);
+			$course_time_end = substr($row['course_time_end'], 0, 5);
+			$ngo_time_start = substr($row['ngo_time_start'], 0, 5);
+			$ngo_time_end = substr($row['ngo_time_end'], 0, 5);
+		}
+	}
+
+
+	if (strpos($category, 'parttime') !== false) {
+		$query2 = "SELECT * FROM `job_seeker_details` WHERE id='$id'";
+		$res2 = $conn->query($query2);
+		if ($res2->num_rows > 0) {
+			while ($row = $res2->fetch_assoc()) {
+				$summary = ucfirst($row['summary']);
+				$field = ucfirst(strtolower($row['field']));
+				$position = ucfirst(strtolower($row['position']));
+				$temp = $row['place_of_work'];
+				if ($temp == "wfh")
+					$place_of_work = 'Work from home';
+				else
+					$place_of_work = 'In Ofiice';
+			}
+		}
+	}
+	if (strpos($category, 'course') !== false) {
+		$query3 = "SELECT * FROM `course_details` WHERE id='$id'";
+		$res3 = $conn->query($query3);
+		if ($res3->num_rows > 0) {
+			while ($row = $res3->fetch_assoc()) {
+
+				$course_field = ucfirst(strtolower($row['field']));
+				$course_name = ucfirst(strtolower($row['course']));
+				$temp = $row['mode_of_learning'];
+				if ($temp == "online")
+					$mode_of_learning = 'Online';
+				else
+					$mode_of_learning = 'Coaching Center';
+			}
+		}
+	}
+
+	?>
 	<!-- Page Preloder -->
 	<div id="preloder">
 		<div class="loader"></div>
@@ -48,17 +102,11 @@
 						<li class="menu-active"><a href="../index.html">Home</a></li>
 						<li><a href="../about-us.html">About Us</a></li>
 						<li><a href="../category.html">Category</a></li>
-						<li><a href="../price.html">Price</a></li>
+
 						<li><a href="../blog-home.html">Blog</a></li>
 						<li><a href="../contact.html">Contact</a></li>
-						<li class="menu-has-children"><a href="">Pages</a>
-							<ul>
-								<li><a href="../elements.html">elements</a></li>
-								<li><a href="../search.html">search</a></li>
-								<li><a href="../single.html">single</a></li>
-							</ul>
-						</li>
-						<li class="menu-has-children"><a href=""><i class='fas fa-user-circle' style='font-size:36px;color:white;'></i></a>
+
+						<li class="menu-has-children" style='background-color:black'><a href=""><i class='fas fa-user-circle' style='font-size:36px;color:white;'></i></a>
 							<ul>
 								<li><a href="../elements.html">Profile</a></li>
 								<li><a href="../search.html">Edit</a></li>
@@ -78,18 +126,17 @@
 					<div class="row">
 						<div class="col-lg-6">
 							<div class="hero-text">
-								<h2>Maria Williams</h2>
-								<p>I’m a digital designer in love with photography, painting and discovering new worlds
-									and cultures.</p>
+								<h2><?php echo $name ?></h2>
+								<p><?php echo "$summary"; ?></p>
 							</div>
 							<div class="hero-info">
 								<h2>General Info</h2>
 								<ul>
-									<li><span>Date of Birth</span>Aug 25, 1988</li>
-									<li><span>Gender</span>Female</li>
-									<li><span>Martial-Status</span>Single</li>
-									<li><span>Address</span>Rosia Road 55, Gibraltar, UK</li>
-									<li><span>PIN code</span>522034</li>
+									<li><span>Age</span><?php echo $age ?></li>
+									<li><span>Gender</span><?php echo $gender ?></li>
+									<li><span>Martial-Status</span><?php echo $status ?></li>
+									<li><span>Address</span><?php echo $address ?></li>
+									<li><span>PIN code</span><?php echo $pin_code ?></li>
 									<li><span>E-mail</span>mariawilliams@company.com</li>
 									<li><span>Phone </span>+43 5266 22 345</li>
 								</ul>
@@ -101,7 +148,15 @@
 						</div>
 						<div class="col-lg-6 text-center">
 							<figure class="hero-image">
-								<img src="img/hero-f.jpg" alt="5">
+								<?php
+								if ($gender == 'Male') {
+								?>
+									<img src="img/hero-m.png" alt="5">
+								<?php
+								} else {
+								?>
+									<img src="img/hero-f.jpg" alt="5">
+								<?php } ?>
 							</figure>
 							<button class='btn btn-dark mt-2'>Upload/Edit</button>
 						</div>
@@ -148,11 +203,37 @@
 					</div>
 					<ul class="resume-list">
 						<li>
-							<h2>Part Time Jobs</h2>
-							<h3>Web Developement</h3>
-							<h4>Full Stack Developer</h4>
-							<p>Place Of Work - Work From Home</p>
-							<p>Time allocated : 13:00pm to 16:00pm</p>
+							<?php
+							if (strpos($category, 'parttime') !== false) {
+								echo "<h2>Part Time Jobs</h2>";
+								echo "<h3>Field -" . $field . "</h3>";
+								echo "<h4>Position -" . $position . "</h4>";
+								echo "<p>Place Of Work -" . $place_of_work . "</p>";
+								echo "<p>Time allocated :" . $part_time_start . " - " . $part_time_end . "</p>";
+							}
+							?>
+						</li>
+						<li>
+							<?php
+							if (strpos($category, 'course') !== false) {
+								echo "<h2>Learn a course</h2>";
+								echo "<h3>Field -" . $course_field . "</h3>";
+								echo "<h4>Position -" . $course_name . "</h4>";
+								echo "<p>Place Of Work -" . $mode_of_learning . "</p>";
+								echo "<p>Time allocated :" . $course_time_start . " - " . $course_time_end . "</p>";
+							}
+							?>
+						</li>
+						<li>
+							<?php
+							if (strpos($category, 'course') !== false) {
+								echo "<h2>Work for a NGO</h2>";
+								echo "<h3>Sector -" . $ngo_field . "</h3>";
+								echo "<h4>Position -" . $ngo_position . "</h4>";
+								echo "<p>Place Of Work -" . $ngo_place_of_work . "</p>";
+								echo "<p>Time allocated :" . $ngo_time_start . " - " . $ngo_time_end . "</p>";
+							}
+							?>
 						</li>
 					</ul>
 				</div>
