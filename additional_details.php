5faf8dd2366bc2689f507a978c2eddacfb1be04d
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-07-09 19:44:54 +0530

    Changed Icons

--- additional_details.php
+++ additional_details.php
@@ -61,7 +61,7 @@ if ($res->num_rows > 0) {
         <div class="container">
             <div class="row align-items-center justify-content-between d-flex">
                 <div id="logo">
-                    <a href="index.html" style="font-size:25px;color:white;font-weight: bold;">NO FULL TIME</a>
+                    <a href="index.html"><img src="img/logo.png" alt="" title="" /></a>
                 </div>
                 <nav id="nav-menu-container">
                     <ul class="nav-menu">
