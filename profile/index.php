d60d8c4ffe1e18b251e33ab9c3fcbafbc0a3324c
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-07-11 12:16:36 +0530

    ADDED LOGOUT FEATURE FOR SEEKERS

--- profile/index.php
+++ profile/index.php
@@ -16,6 +16,8 @@
 	<link rel="stylesheet" href="css/magnific-popup.css" />
 	<link rel="stylesheet" href="css/style.css" />
 	<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+	<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="../css/linearicons.css">
 	<link rel="stylesheet" href="../css/bootstrap.css">
 	<link rel="stylesheet" href="../css/magnific-popup.css">
