f8be7833c7af92a36b21c8301239791ed46fb448
TishaMogha <tisha280402@gmail.com>
2021-07-12 23:33:39 +0530

    --

--- joblister/config/config.php
+++ joblister/config/config.php
@@ -3,7 +3,7 @@
 define("DB_HOST","localhost");
 define("DB_USER","root");
 define("DB_PASS","Tisha@28");
-define("DB_NAME","joblister");
+define("DB_NAME","nofulltime");
 
 
 define("SITE_TITLE","JobLister");
