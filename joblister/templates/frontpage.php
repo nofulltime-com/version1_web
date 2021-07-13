445b20e70c82147a62669e12b077e5bb6d55f89e
TishaMogha <tisha280402@gmail.com>
2021-07-13 12:27:37 +0530

    joblister

--- joblister/templates/frontpage.php
+++ joblister/templates/frontpage.php
@@ -17,7 +17,19 @@
             <h3 class="text-muted"><?php echo SITE_TITLE; ?></h3>
         </div><hr/>
 <?php displayMessage();?>
-    
+        <div class="jumbotron">
+                <h1>Find A Job</h1>
+                <form method="GET" action="index.php">
+                    <select name="category" class="form-control">
+                        <option value="0">Choose Category</option>
+                        <?php foreach($categories as $category): ?>
+                            <option value="<?php echo $category->id; ?>"><?php echo $category->name; ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                    <br>
+                    <input type="submit" class="btn btn-lg btn-success" value="FIND">
+                </form>
+        </div>
         <h3><?php echo $title; ?></h3>
             <?php foreach($jobs as $job): ?>
                 <div class="row marketing">
