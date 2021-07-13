c776b63efc01bd600efa50f583ef9baa529a2190
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-07-13 12:13:10 +0530

    Working on home page

--- joblister/config/config.php
+++ joblister/config/config.php
@@ -1,9 +1,9 @@
 <?php
 //DB params
-define("DB_HOST","localhost");
-define("DB_USER","root");
-define("DB_PASS","Tisha@28");
-define("DB_NAME","nofulltime");
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "3183");
+define("DB_NAME", "nofulltime");
 
 
-define("SITE_TITLE","JobLister");
+define("SITE_TITLE", "JobLister");
