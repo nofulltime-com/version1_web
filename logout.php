641e3aea9ed4c9a77b7442c709db8603d070a107
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-07-16 20:02:50 +0530

    Improved UI in seeker dashboard

--- logout.php
+++ logout.php
@@ -1,3 +1,5 @@
 <?php
+session_start();
 unset($_SESSION['id']);
+
 header("Location: ./Register/studentLogin.php");
--- profile/index.php
+++ profile/index.php
@@ -220,10 +220,12 @@
 									<li><span>E-mail</span><?php echo $email ?></li>
 									<li><span>Phone </span><?php echo $phno ?></li>
 								</ul>
-								<div class="col-md-8 mt-5 text-md-right header-buttons">
-									<a href="../uploads/resume/Bhanu Krishna Prasad-Resume.pdf" class="site-btn" download>Download Resume</a>
-									<a href="#contact" class="site-btn">Contact Me</a>
-								</div>
+								<?php if (isset($_SESSION['recruiter_id'])) { ?>
+									<div class="col-md-8 mt-5 text-md-right header-buttons">
+										<a href="../uploads/resume/Bhanu Krishna Prasad-Resume.pdf" class="site-btn" download>Download Resume</a>
+										<a href="#contact" class="site-btn">Contact Me</a>
+									</div>
+								<?php } ?>
 							</div>
 						</div>
 						<div class="col-lg-6 text-center">
@@ -238,7 +240,9 @@
 									<img src="img/hero-f.jpg" alt="5">
 								<?php } ?>
 							</figure>
-							<button class='btn btn-dark mt-2'>Upload/Edit</button>
+							<?php if (!isset($_SESSION['recruiter_id'])) { ?>
+								<button class='btn btn-dark mt-2'>Upload/Edit</button>
+							<?php } ?>
 						</div>
 					</div>
 				</div>
@@ -331,35 +335,37 @@
 				</div>
 			</div>
 			</section>
-			<!-- Contact section start -->
-			<section class="contact-section spad" id='contact'>
-				<div class="container-fluid">
-					<div class="row">
-						<div class="col-xl-8 offset-xl-2">
-							<div class="section-title">
-								<h2>Contact Me</h2>
+			<?php if (isset($_SESSION['recruiter_id'])) { ?>
+				<!-- Contact section start -->
+				<section class="contact-section spad" id='contact'>
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-xl-8 offset-xl-2">
+								<div class="section-title">
+									<h2>Contact Me</h2>
+								</div>
+								<form class="contact-form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" autocomplete="off">
+									<div class="row">
+										<div class="col-md-6">
+											<input name="name" type="text" placeholder="Name">
+										</div>
+										<div class="col-md-6">
+											<input type="text" name="email" placeholder="E-mail">
+										</div>
+										<div class="col-md-12">
+											<input type="text" name="subject" placeholder="Subject">
+											<textarea placeholder="Message" name="message"></textarea>
+										</div>
+									</div>
+									<div class="text-md-right">
+										<button class="site-btn" name='mail' type='submit'>Send message</button>
+									</div>
+								</form>
 							</div>
-							<form class="contact-form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" autocomplete="off">
-								<div class="row">
-									<div class="col-md-6">
-										<input name="name" type="text" placeholder="Name">
-									</div>
-									<div class="col-md-6">
-										<input type="text" name="email" placeholder="E-mail">
-									</div>
-									<div class="col-md-12">
-										<input type="text" name="subject" placeholder="Subject">
-										<textarea placeholder="Message" name="message"></textarea>
-									</div>
-								</div>
-								<div class="text-md-right">
-									<button class="site-btn" name='mail' type='submit'>Send message</button>
-								</div>
-							</form>
 						</div>
 					</div>
-				</div>
-			</section>
+				</section>
+			<?php } ?>
 			<footer class="footer-area section-gap">
 				<div class="container">
 					<div class="row">
