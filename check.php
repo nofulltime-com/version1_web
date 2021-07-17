e671d35d8fc30e8e51ca5d8699718d2bd0c1673e
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-07-17 18:59:22 +0530

    Improved UI

---
+++ check.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+include './connect.php';
+$id = $_SESSION['id'];
+$query = "SELECT * FROM `seeker_details` WHERE id='$id'";
+$res = $conn->query($query);
+if ($res->num_rows > 0)
+    header("Location: ./profile/index.php");
+else
+    header("Location: ./seeker_preferences.php");
