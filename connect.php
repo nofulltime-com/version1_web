e5df94aee9d2fbfb2629ec78ae2accdee5ccf7f0
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-07-22 23:38:29 +0530

    Improved UI

--- connect.php
+++ connect.php
@@ -8,4 +8,15 @@ if ($conn->connect_error)
 
 echo "<script>console.log('Connected to DB');</script>";
 
-//job lister file
+
+// <?php
+// $username = "nofulltime_nofulltime";
+// $servername = "localhost";
+// $password = "Intens@1555!";
+// $conn = new mysqli($servername, $username, $password, "nofulltime_nofulltime");
+// if ($conn->connect_error)
+//     die("Connection Error");
+
+// echo "<script>console.log('Connected to DB');</script>";
+
+// //job lister file
