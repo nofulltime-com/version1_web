f2239b81937ce712d8c7171d57ef6500cfa862d6
Tisha Mogha <86661488+Tishamogha@users.noreply.github.com>
2021-07-26 15:27:33 +0530

    Add files via upload

---
+++ recruiters_details/connect.php
@@ -0,0 +1,25 @@
+<?php
+	Session_start();
+	$id = $_SESSION['recruiter_id'];
+	$name = $_POST['name'];
+	$organization = $_POST['organization'];
+	$designation = $_POST['designation'];
+	$location = $_POST['location'];
+	$industry = $_POST['industry'];
+	$subscription_mode = 'blank';
+	$id = 1;
+
+    // Database Connection
+    $conn = new mysqli('localhost','root','Tisha@28','nofulltime_nofulltime');
+
+	if($conn->connect_error){
+		die("Connection Failed : ". $conn->connect_error);
+    } else {
+	 	$stmt = $conn->prepare("INSERT into recruiter_details(id, name, organization, designation, location, industry, subscription_mode) values(?, ?, ?, ?, ?, ?, ?)");
+	 	$stmt->bind_param("issssss", $id, $name, $organization, $designation, $location, $industry, $subscription_mode);
+		$stmt->execute();
+		echo "Details Registered Successfully...!";
+		$stmt->close();
+		$conn->close();
+	}
+?>
