844f91ed21406f406fae2c55f3f098dd4594bf5b
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-07-26 21:10:36 +0530

    DATABASE CREDENTIALS

--- connect.php
+++ connect.php
@@ -1,18 +1,19 @@
-<!-- $username = "root";
-$servername = "localhost";
-$password = "Tisha@28";
-$conn = new mysqli($servername, $username, $password, "nofulltime_nofulltime");
-if ($conn->connect_error)
-    die("Connection Error");
-
-echo "<script>console.log('Connected to DB');</script>"; -->
-
-
 <?php
-$username = "nofulltime_nofulltime";
+$username = "root";
 $servername = "localhost";
-$password = "Intens@1555!";
-$conn = new mysqli($servername, $username, $password, "nofulltime_nofulltime");
+$password = "3183";
+$conn = new mysqli($servername, $username, $password, "nofulltime");
 if ($conn->connect_error)
     die("Connection Error");
+
 echo "<script>console.log('Connected to DB');</script>";
+
+
+
+// $username = "nofulltime_nofulltime";
+// $servername = "localhost";
+// $password = "Intens@1555!";
+// $conn = new mysqli($servername, $username, $password, "nofulltime_nofulltime");
+// if ($conn->connect_error)
+//     die("Connection Error");
+// echo "<script>console.log('Connected to DB');</script>";
