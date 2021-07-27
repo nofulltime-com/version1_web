e8ee0c442f7582563a664545cd0628382d729065
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-07-27 15:35:13 +0530

    updated nav and footer links

--- joblister/templates/job-single.php
+++ joblister/templates/job-single.php
@@ -17,10 +17,8 @@
                     <ul class="nav-menu">
                         <li class="menu-active"><a href="../index.php">Home</a></li>
                         <li><a href="../about-us.php">About Us</a></li>
-                        <li><a href="../category.php">Category</a></li>
-                        <li><a href="../recruiters.php">recruiters</a></li>
-                        <li><a href="../blog-home.php">Contact</a></li>
-
+                        <li><a href="../blog-home.php">Blogs</a></li>
+                        <li><a href="../contact.php">Contact</a></li>
                         <?php
                         if (isset($_SESSION['id'])) { ?>
                             <li class="menu-has-children" style='background-color:none'><a href=""><i class='fas fa-user-circle' style='font-size:36px;color:white'></i></a>
@@ -101,8 +99,6 @@
                         <ul class="footer-nav">
                             <li><a href="../index.php">Home</a></li>
                             <li><a href="../about-us.php">About Us</a></li>
-                            <li><a href="../category.php">Category</a></li>
-                            <li><a href="../recruiters.php">Recruiters</a></li>
                             <li><a href="../blog-home.php">Blog</a></li>
                         </ul>
                     </div>
