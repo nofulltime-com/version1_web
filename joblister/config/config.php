93c5a9edfb4750f4de94edfe0e34978484723986
TishaMogha <tisha280402@gmail.com>
2021-07-30 00:09:30 +0530

    Changes

--- joblister/config/config.php
+++ joblister/config/config.php
@@ -2,8 +2,8 @@
 //DB params
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "3183");
-define("DB_NAME", "nofulltime");
+define("DB_PASS", "Tisha@28");
+define("DB_NAME", "nofulltime_nofulltime");
 
 
 define("SITE_TITLE", "NoFullTime");
