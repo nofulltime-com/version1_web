2b7eecd0ed1ac9b8af97841ce1543eb4476ef95c
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-08-06 08:27:55 +0530

    Changed Mail Id in contact form

--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST['submit'])) {
-    $to = "bhanukrishnaprasad3333@gmail.com";
+    $to = "nofulltime.abc@gmail.com";
     $from = $_POST['email'];
     $name = $_POST['name'];
     $subject = $_POST['subject'];
