7306092165cb1fb6e370a8540f60f2b33278dd1a
TishaMogha <tisha280402@gmail.com>
2021-08-10 13:51:22 +0530

    Job seekers

---
+++ jobseekers/config/config.php
@@ -0,0 +1,9 @@
+<?php
+//DB params
+define("DB_HOST","localhost");
+define("DB_USER","root");
+define("DB_PASS","Tisha@28");
+define("DB_NAME","nofulltime_nofulltime");
+
+
+define("SITE_TITLE","JobLister");
---
+++ jobseekers/templates/job-single.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <style>
+            <?php include "../templates/inc/css/main2.css"?>
+            <?php include "../templates/inc/css/narrow.css"?>
+            <?php include "./inc/css/footer.css"?>
+   </style>
+        <div class="container">
+            <div class="head clearfix">
+                <div class="head">
+                <ul class="head head-pills pull-right">
+                    <li role="presentation" style=background-color:#49e4fa;border-radius:20px;padding-left:20px;padding-right:20px;><a href="index.php">Home</a></li>
+                </ul>
+            </div>
+            <h3 class="text-muted"><?php echo SITE_TITLE; ?></h3>
+        </div><hr/>
+<?php displayMessage();?>
+        <h2 class="page-header"><?php echo $job->job_title; ?> (<?php echo $job->location; ?>) </h2> 
+        <small style=font-size:18px;>Posted By <?php echo $job->contact_user; ?> on <?php echo $job->post_date; ?></small>
+        <hr>
+	    <p class="lead"><?php echo $job->description; ?></p>
+        <ul class="list-group">
+		<li class="list-group-item" style=font-size:18px;><strong>Company:</strong> <?php echo $job->company; ?></li>
+		<!-- <li class="list-group-item" style=font-size:18px;><strong>Salary:</strong> <?php echo $job->salary; ?></li> -->
+        <li class="list-group-item"><strong>Contact Email:</strong> <?php echo $job->contact_email; ?></li>
+	    </ul>
+        <br><br>
+	    <a href="index.php" style=color:#a367e7;font-size:20px;font-weight:500;>Go Back</a>
+	    <br><br>
+        
+
+        <footer class="footer">
+                    <p>&copy; 2021,No-full-time.com</p>
+                </footer>
+                </div> <!-- /container -->
+            </body>
+        </html>
