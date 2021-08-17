b49627c8af05a6910eaffe9972b60e32e156fd20
KevanMehta <kevanmehta2511@gmail.com>
2021-08-17 16:05:25 +0530

    Added Email verification and updated google login code

---
+++ Register/user-verify.php
@@ -0,0 +1,75 @@
+<?php 
+// $email = $_SESSION['email'];
+// if($email == false){
+//   header('Location: studentLogin.php');
+session_start();
+// }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Code Verification</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="studentRegister.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form">
+                <form action="user-verify.php" method="POST" autocomplete="off">
+                    <h2 class="text-center">Email Verification</h2>
+                    <?php 
+                    if(isset($_SESSION['info'])){
+                        ?>
+                        <div class="alert alert-success text-center">
+                            <?php echo $_SESSION['info']; ?>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                   <?php if (isset($_GET['error'])) { ?>
+                    <p class="error"><?php echo $_GET['error']; ?></p>
+                    <?php } ?>
+                    <div class="form-group">
+                        <input class="form-control" type="number" name="otp" placeholder="Enter verification code" required>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control button" type="submit" name="check" value="Submit">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <?php
+    include 'dbcon.php';
+    
+    if(isset($_POST['check'])){
+        $_SESSION['info'] = "";
+        $otp_code = mysqli_real_escape_string($conn, $_POST['otp']);
+        $check_code = "SELECT * FROM users WHERE code = $otp_code";
+        $code_res = mysqli_query($conn, $check_code);
+        if(mysqli_num_rows($code_res) > 0){
+            $fetch_data = mysqli_fetch_assoc($code_res);
+            $fetch_code = $fetch_data['code'];
+            $email = $fetch_data['email'];
+            $code = 0;
+            $status = 'verified';
+            $update_otp = "UPDATE users SET code = $code, status = '$status' WHERE code = $fetch_code";
+            $update_res = mysqli_query($conn, $update_otp);
+            if($update_res){
+                $_SESSION['name'] = $name;
+                $_SESSION['email'] = $email;
+                header('location: studentLogin.php');
+                exit();
+            }else{
+                header("Location: user-verify.php?error=Failed while updating code!");
+            }
+        }else{
+            header("Location: user-verify.php?error=You have entered Incorrect Code!");
+        }
+    }
+    ?>
+</body>
+</html>
