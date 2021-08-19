f35b78ec119460415442ae8aaa4a6c04958ba3ac
KevanMehta <kevanmehta2511@gmail.com>
2021-08-19 19:01:01 +0530

    Updates

--- Register/user-verify.php
+++ Register/user-verify.php
@@ -11,12 +11,56 @@ session_start();
     <meta charset="UTF-8">
     <title>Code Verification</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="studentRegister.css">
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="font-awesome.min.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <link rel="stylesheet" href="../css/linearicons.css">
+    <link rel="stylesheet" href="../css/magnific-popup.css">
+    <link rel="stylesheet" href="../css/nice-select.css">
+    <link rel="stylesheet" href="../css/animate.min.css">
+    <link rel="stylesheet" href="../css/owl.carousel.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="./seeker_details.css">
+
+    
     <link rel="stylesheet" href="studentRegister.css">
 </head>
 <body>
-    <div class="container">
-        <div class="row">
+<header id="header" id="home" style="background-color: black;">
+        <div class="container">
+            <div class="row align-items-center justify-content-between d-flex">
+                <div id="logo">
+                    <a style="color:white ;font-size: x-large;font-weight:bold" href="../index.php"><span><i class="fas fa-dove"></i></span> Nofulltime</a>
+                </div>
+                <nav id="nav-menu-container">
+                    <ul class="nav-menu">
+                        <li class="menu-active"><a href="../index.php">Home</a></li>
+                        <li><a href="../about-us.php">About Us</a></li>
+                        <li><a href="../blog-home.php">Blogs</a></li>
+                        <li><a href="../contact.php">Contact</a></li>
+                        <li class="menu-has-children" style='background-color:white'><a href="" style='color:#7b63f1'>Signup</a>
+                            <ul>
+                                <li><a href="studentRegister.php">As a Part-timer</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-has-children" style='background-color:white'><a href="" style='color:#7b63f1'>Login</a>
+                            <ul>
+                                <li><a href="studentLogin.php">As a Part-timer</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav><!-- #nav-menu-container -->
+            </div>
+        </div>
+    </header>
+    <div class="container" style="padding:130px; padding-bottom:50px">
+        <div class="row"> 
             <div class="col-md-4 offset-md-4 form">
                 <form action="user-verify.php" method="POST" autocomplete="off">
                     <h2 class="text-center">Email Verification</h2>
@@ -71,5 +115,63 @@ session_start();
         }
     }
     ?>
+    <footer class="footer-area section-gap">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3  col-md-12">
+                    <div class="single-footer-widget">
+                        <h6>Short Links</h6>
+                        <ul class="footer-nav">
+                            <li><a href="../index.php">Home</a></li>
+                            <li><a href="../about-us.php">About Us</a></li>
+                            <li><a href="../blog-home.php">Blogs</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-6  col-md-12">
+                    <div class="single-footer-widget newsletter">
+                        <h6>Other Links</h6>
+                        <ul class="footer-nav">
+                            <li><a href="../terms.php">Terms and Conditions</a></li>
+                            <li><a href="../contact.php">Contact</a></li>
+                        </ul>
+                        <br>
+
+                    </div>
+                </div>
+                <div class="col-lg-3  col-md-12">
+                    <div class="single-footer-widget mail-chimp">
+                        <h6>Get In Touch</h6>
+                        <a title="Address "><i class="fa fa-map-marker "></i> No.32, Vinayagar Kovil Street,
+                            Karungalpalayam,
+                            Erode - 638003, Tamil Nadu, India</a>
+                        <br>
+                        <a href="mailto:support@nofulltime.com " title="Email "><i class="fa fa-envelope "></i>
+                            support@nofulltime.com</a>
+                        <br>
+                        <a href="tel:+919790030919 " title="Contact "><i class="fa fa-phone "></i> +91-97900-30919</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row footer-bottom d-flex justify-content-between">
+                <p class="col-lg-8 col-sm-12 footer-text m-0 text-white">
+
+                    Copyright &copy;
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script> NoFullTime |All rights reserved
+                </p>
+                <div class="col-lg-4 col-sm-12 footer-social">
+                    <a href="https://www.facebook.com/abcinfomediapvtltd/"><i class="fa fa-facebook"></i></a>
+                    <a href="https://www.instagram.com/abc_infomedia/"><i class="fa fa-instagram"></i></a>
+                    <a href="https://twitter.com/abc_infomedia/"><i class="fa fa-twitter"></i></a>
+                    <a href="https://in.linkedin.com/company/abc-infomedia-private-limited"><i class="fa fa-linkedin"></i></a>
+                    <a href="https://in.pinterest.com/abcinfomediapvtltd/"><i class="fa fa-pinterest"></i></a>
+                    <a href="https://www.youtube.com/channel/UCa5hWmytEVKaeyqI_jpgAlw"><i class="fa fa-youtube"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
